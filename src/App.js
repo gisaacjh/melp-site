9d8b249485d44373689b2509cfd8109a8dc9009e
Isaac <gisaacjh@outlook.com>
2018-01-09 18:32:41 -0600

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import { Switch, Route } from 'react-router-dom';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <h2>hi twhis works </h2>
+      </div>
+    );
+  }
+}
+
+export default App;
